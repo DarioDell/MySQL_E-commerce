5a334869d960710a898d3ed69e6f5e141e29bc11
Dario Dell' Agostino <dariodellagostino@gmail.com>
2024-08-10 19:27:16 +0200

    Add files via upload
    The differents tables

---
+++ Tables.sql
@@ -0,0 +1,32 @@
+USE import;
+
+SELECT * FROM customers;   
+-- 'creditLimit': creditLimit refers to the maximum amount of credit that has been assigned to each client. 
+-- This credit limit indicates the maximum amount that a customer can owe the company at any given time.
+
+SELECT * FROM employees;
+
+SELECT * FROM offices;
+
+SELECT * FROM orderdetails;
+
+SELECT * FROM orders;     
+
+SELECT * FROM payments;
+
+SELECT * FROM productlines;
+       
+SELECT * FROM products; 		
+-- 'buyPrice' = Is the price that the company paid for the product under evaluation. This price is vital in establishing the profitability of the products 
+-- to ensure that the business is making enough profits to break even. Because it directly influences the profit margin, it is the portion of what a company sells.
+-- 'MSRP'(Manufacturer's Suggested Retail Price ) = It is the price the manufacturer recommends retailers charge for a product. This price is essential to determine profitability 
+-- of the products that a company sells, since it directly affects the profit margin.
+
+SELECT * FROM warehouses;  
+-- 'warehousePctCap' refers to the percentage of storage capacity used in a warehouse. It is a metric that indicates how full or busy 
+-- is a warehouse in relation to its total capacity. This percentage helps companies efficiently manage their storage space, 
+-- plan the receipt and shipment of merchandise, and make decisions about the need to expand or reduce storage space.
+                            
+                            
+                            
+   
